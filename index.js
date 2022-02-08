a76c63e65322da9fe918f4b7ef7877e47ab6b2e8
“Daysha <+>
2022-02-08 12:26:32 -0800

    commit tree c3758ba38aa91035d9099b7dc55a8b9900d7d1ca

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Your code here
+
+
+function createEmployeeRecord(info) {
+const [firstName, familyName, title, payPerHour] = info;
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+};
+
+function createEmployeeRecords(empRecords) {
+    return empRecords.map(emp => createEmployeeRecord(emp));
+};
+
+function createTimeInEvent(employeeRecord, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+  
+    employeeRecord.timeInEvents.push({
+         type: "TimeIn",
+         hour: parseInt(hour, 10),
+         date,
+     })
+     return employeeRecord
+  };
+  
+  
+function createTimeOutEvent(employeeRecord, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employeeRecord
+};
+
+function  hoursWorkedOnDate(employeeRecord, date){
+    let inEvent =  employeeRecord.timeInEvents.find(
+        d =>  d.date === date)
+    let outEvent =  employeeRecord.timeOutEvents.find(
+        d =>  d.date === date)
+    return (outEvent.hour - inEvent.hour)/100
+};
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    return employeeRecord.payPerHour * hoursWorkedOnDate(employeeRecord, date);
+};
+
+function allWagesFor(employeeRecord){
+    const allWages = employeeRecord.timeInEvents.map((day) => {return wagesEarnedOnDate(employeeRecord, day.date)})
+    return allWages.reduce((a, b) => a + b, 0)
+};
+
+function findEmployeeByFirstName(employeeRecords, firstName) {
+    return employeeRecords.find((record) => record.firstName === firstName)
+};
+
+function calculatePayroll(employeeRecords){
+    const allPay = (employeeRecords.map((e) => {return allWagesFor(e)}))
+    return allPay.reduce((a, b) => a + b, 0)
+};
